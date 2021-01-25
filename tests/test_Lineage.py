65ca008534519c2d0557c70af7ced0d03d15af83
Richard de Borja <rdeborja@oicr.on.ca>
2021-01-25 10:20:51 -0500

    updated tests to include get_sample_name

--- tests/test_Lineage.py
+++ tests/test_Lineage.py
@@ -14,4 +14,12 @@ class LineageTest(unittest.TestCase):
         self.assertEqual(lineage_dict['sampleA'], 'B.1.1.43')
         self.assertEqual(lineage_dict['sampleB'], 'B.1.36')
         self.assertEqual(lineage_dict['sampleC'], 'B.1.1.7')
-
+    def test_get_sample_name(self):
+        sample_row = {'taxon' : 'sampleA/ARTIC/nanopolish',
+                      'lineage' : 'B.1.1.43',
+                      'probability' : 1.0,
+                      'pangoLEARN_version' : '2021-01-06',
+                      'passed_qc' : 'passed_qc'}
+        expected_sample_name = 'sampleA'
+        sample_name = test_lineage.get_sample_name(row=sample_row)
+        self.assertEqual(sample_name, expected_sample_name)
