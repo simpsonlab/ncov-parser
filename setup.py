224c086f58294529c2adbf35ef6f3be24b95fd62
Richard de Borja <rdeborja@oicr.on.ca>
2022-04-26 09:02:35 -0400

    updated version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ncov_parser",
-    version="0.6.9",
+    version="0.7.0",
     author="Richard J. de Borja",
     author_email="richard.deborja@oicr.on.ca",
     description="A nCoV package for parsing analysis files",
